c6bb2bad52906a518f883e928527bc0a3dd44284
MOHAMMAD AFFAN <mohammad.affan210@gmail.com>
2025-04-17 17:06:09 +0530

    initial ReAct code commit

--- main.py
+++ main.py
@@ -1,56 +1,192 @@
-import os
 from dotenv import load_dotenv
-from typing import Tuple, Dict, Any
+from typing import Union, List, Tuple
+from langchain.schema import AgentAction, AgentFinish
 
-from langchain_core.prompts import PromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-
-from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
 from langchain_ollama import ChatOllama
-from langchain_ollama.llms import OllamaLLM
 
-from third_parties.linkedin import scrape_linkedin_profile
-from agents.linkedin_lookup_agent import linkedin_lookup
-from agent_tools.output_parser import summary_parser, Summary
+from langchain.tools import tool, Tool
+from langchain.tools.render import render_text_description_and_args, render_text_description
+from langchain.agents.output_parsers import ReActSingleInputOutputParser
 
-def ice_break_linked_url(name: str) -> Tuple[Summary, str]:
-    linkedin_username = linkedin_lookup(name=name)
-    linkedin_info = scrape_linkedin_profile(url = linkedin_username)
+from agent_tools.custom_callbacks import CustomAgentCallbackHandler
+from agent_tools.output_parser import text_length_parser, FinalAnswerModel
 
-    query_template = """given the LinkedIn profile information {information} of a person, I want you to create:
-     1. a short summary of the person in 2-3 sentences
-     2. interesting facts about the person
-     Return the result in the following JSON format:
-    {{
-        "summary": "<A brief summary of the person>",
-        "facts": ["<Interesting fact 1>", "<Interesting fact 2>", ...]
-    }}
-    """
-    #  \n{format_instructions}""" # not working with gemma2, hence manual prompt for output formating needed (line 23-27) to comply with pydantic summary_parser object
-    
-    query_prompt_template = PromptTemplate(
-        input_variables=["information"], 
-        template=query_template,
-        # partial_variables={"format_instructions": summary_parser.get_format_instructions()}, # not working with gemma2
-    )
+@tool
+def get_text_length(text: str, topic: str) -> int:
+    """Get the length of a string."""
+    # print(f"get_text_length called with text: {text}")
+    text = text + topic # concatenating the text and topic
+    text = text.strip("\n").strip('"') # stripping non-alphanumeric characters
+    return len(text)
 
-    llm = ChatOllama(temperature=0, model="gemma2:2b")
+def find_tool_by_name(tools: List[Tool], tool_name: str) -> Tool:
+    """Find a tool by its name."""
+    for tool in tools:
+        if tool.name == tool_name:
+            return tool
+    raise ValueError(f"Tool with name {tool_name} not found.")
 
-    chain = query_prompt_template | llm | summary_parser # | StrOutputParser() # this is a chain of components that will be executed in order 
-
-    res = chain.invoke(input={"information": linkedin_info})
-    print(res)
-    print(res.to_dict())
-
-    return res, linkedin_info.get("photoUrl")
+def format_log_str(
+        intermediate_steps: List[Tuple[AgentAction, str]], 
+        observation_prefix: str = "Observation:", 
+        llm_prefix: str = "Thought:"
+    ) -> str:
+    """ Construct the scratchpad that lets the agent continue its thought process """
+    thoughts = ""
+    for action, observation in intermediate_steps:
+        thoughts += action.log
+        thoughts += f"\n{observation_prefix} {observation}\n{llm_prefix}"
+    # print(f"format_log_str: {thoughts=}")
+    return thoughts
 
 if __name__ == "__main__":
     load_dotenv()
-    print("Hello LinkedIn LangChain!")
-    # print(f"{os.environ['OPENAI_API_KEY']}")
-    print("-"*20)
-    ice_break_linked_url(name="Eden Marco")
+    print("\nHello LangChain ReAct Agent!\n")
+    # res = get_text_length.invoke(input={"text": "topic", "topic": "hello world"})
+    
+    user_input_to_llm = "What is the length of the text: dogg ? I only need the number"
 
-    # linkedin_info = scrape_linkedin_profile("https://www.linkedin.com/in/eden-marco/")
+    harrison_chase_modified_prompt = """
+        You have access to the following tools:
+        {tools}
 
+        Please use the following format:
 
+        Question: the input question you must answer
+        Thought: Do I need to use a tool? Yes/No I need/need not to use a tool
+        Action: the action to take, should be one of [{tool_names}]
+        Action Input: the input to the action
+        Observation: the result of the action
+        ... (this Thought/Action/Action Input/Observation can repeat N times)
+
+        When you have a response to say to the Human, or if you do not need to use a tool, you MUST use the format:
+        {{
+        Thought: I know the final answer, Do I need to use a tool? No
+        Final Answer: [your response or final answer to the original input Question here]
+        }}
+        Do not attach backticks or quotes to any of your outputs, including thoughts, tool calls and final answers.
+
+        Begin!
+
+        Question: {input}
+        Thought: Do I need to use a tool? {agent_scratchpad}
+    """
+    # agent_scratchpad : brings back the previous thought process (history) of the agent
+    
+    llm = ChatOllama(
+        model="gemma2:2b",
+        temperature=0,
+        stop=["\nObservation"],
+        callbacks=[CustomAgentCallbackHandler()],
+        # prompt_template=prompt_template
+    )
+    
+    tools = [
+        get_text_length
+    ]
+    
+    prompt_template = PromptTemplate.from_template(template=harrison_chase_modified_prompt).partial(
+        tools=render_text_description( tools), 
+        tool_names=", ".join([tool.name for tool in tools])
+    )
+    
+    intermediate_steps = []
+
+    agent = ( # | = pipe  LCEL (Langchain Expression Language) operator, which is a way to chain components together in LangChain
+        {
+            "input": lambda x: x["input"],
+            "agent_scratchpad": lambda x: format_log_str(x["agent_scratchpad"]),
+         } 
+         | prompt_template 
+         | llm 
+         | text_length_parser)
+    
+    print(agent)
+
+    """## # initial implementation of the agent executor by myself # ##
+    
+    agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+        {
+            "input": user_input_to_llm,
+            "agent_scratchpad": intermediate_steps
+        }
+    )
+    # print(agent_step)
+    print("Agent Executor Steps:")
+    print(f"{'-'*30}\n")
+    print(agent_step.model_dump().get("log"))
+
+    react_agent_step_counter = 1
+    if isinstance(agent_step, AgentAction):
+        while not isinstance(agent_step, AgentFinish):
+            react_agent_step_counter += 1
+            
+            tool_name = agent_step.tool
+            tool_to_use = find_tool_by_name(tools, tool_name)
+            tool_input = agent_step.tool_input
+
+            observation = tool_to_use.func(str(tool_input), "")  # 2nd argument is the topic of function get_text_length, passing blank as of now
+            # print(f"{observation=}")
+            intermediate_steps.append((agent_step, str(observation)))
+
+            # Re-invoke the agent with the updated intermediate steps and observations
+            agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+                {
+                    "input": user_input_to_llm,
+                    "agent_scratchpad": intermediate_steps
+                }
+            )
+            print(agent_step.model_dump().get("log"))
+            if react_agent_step_counter > 5:
+                print("Too many thought processes, breaking out of the thinking.")
+                break
+        if isinstance(agent_step, AgentFinish):
+            print("-"*30)
+            print(agent_step.return_values.get("output"))
+        #############################################################################
+        """
+    
+    # easy while loop implementation of the agent executor
+    print("Agent Executor Steps:")
+    print(f"{'-'*30}\n")
+    agent_step = ""
+    react_agent_step_counter = 1
+    while not isinstance(agent_step, FinalAnswerModel):
+        react_agent_step_counter += 1
+        # Invoke the agent with the intermediate steps and observations, the loop will reinvoke the agent with the updated intermediate steps and observations
+        agent_step: Union[AgentAction, AgentFinish, FinalAnswerModel] = agent.invoke({
+                "input": user_input_to_llm,
+                "agent_scratchpad": intermediate_steps
+            }
+        )
+        if isinstance(agent_step, AgentAction):
+            print(agent_step.model_dump().get("log"))
+            
+            tool_name = agent_step.tool
+            tool_to_use = find_tool_by_name(tools, tool_name)
+            tool_input = agent_step.tool_input
+
+            observation = tool_to_use.func(str(tool_input), "")  # 2nd argument is the topic of custom function get_text_length which contains the business logic, passing blank as of now
+            intermediate_steps.append((agent_step, str(observation)))
+
+        if react_agent_step_counter > 5: # to avoid infinite loop
+            print("Too many thought processes, breaking out of the thinking.")
+            break
+            
+    if isinstance(agent_step, FinalAnswerModel):
+        print("-"*30)
+        print(agent_step.to_dict())
+        # print(agent_step.return_values.get("output"))
+    
+    # above is the same as below 
+    # from langchain.agents import initialize_agent, AgentType
+    # agent_executor = initialize_agent(
+    #     tools=tools,
+    #     llm=llm,
+    #     agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    #     verbose=True,
+    #     agent_kwargs={
+    #         "agent_scratchpad": format_log_str(intermediate_steps)
+    #     },
+    # )
