d402aa9b87782badf2c2b3f1461e0952b662bfaa
MOHAMMAD AFFAN <mohammad.affan210@gmail.com>
2025-04-19 22:56:03 +0530

    rag chain created in two ways

--- main.py
+++ main.py
@@ -1,192 +1,127 @@
+import os
 from dotenv import load_dotenv
-from typing import Union, List, Tuple
-from langchain.schema import AgentAction, AgentFinish
 
-from langchain.prompts import PromptTemplate
+from langchain_community.document_loaders import TextLoader
+
+from langchain_pinecone import PineconeVectorStore
+from langchain_text_splitters import CharacterTextSplitter
+
+# from langchain_ollama import OllamaEmbeddings
+from langchain_community.embeddings import OllamaEmbeddings
+
 from langchain_ollama import ChatOllama
+from langchain_core.prompts import PromptTemplate
 
-from langchain.tools import tool, Tool
-from langchain.tools.render import render_text_description_and_args, render_text_description
-from langchain.agents.output_parsers import ReActSingleInputOutputParser
+from langchain import hub
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
 
-from agent_tools.custom_callbacks import CustomAgentCallbackHandler
-from agent_tools.output_parser import text_length_parser, FinalAnswerModel
+from langchain_core.runnables import RunnablePassthrough
 
-@tool
-def get_text_length(text: str, topic: str) -> int:
-    """Get the length of a string."""
-    # print(f"get_text_length called with text: {text}")
-    text = text + topic # concatenating the text and topic
-    text = text.strip("\n").strip('"') # stripping non-alphanumeric characters
-    return len(text)
-
-def find_tool_by_name(tools: List[Tool], tool_name: str) -> Tool:
-    """Find a tool by its name."""
-    for tool in tools:
-        if tool.name == tool_name:
-            return tool
-    raise ValueError(f"Tool with name {tool_name} not found.")
-
-def format_log_str(
-        intermediate_steps: List[Tuple[AgentAction, str]], 
-        observation_prefix: str = "Observation:", 
-        llm_prefix: str = "Thought:"
-    ) -> str:
-    """ Construct the scratchpad that lets the agent continue its thought process """
-    thoughts = ""
-    for action, observation in intermediate_steps:
-        thoughts += action.log
-        thoughts += f"\n{observation_prefix} {observation}\n{llm_prefix}"
-    # print(f"format_log_str: {thoughts=}")
-    return thoughts
+def format_docs(docs):
+    """Format the documents for the prompt."""
+    return "\n\n".join([doc.page_content for doc in docs])
 
 if __name__ == "__main__":
     load_dotenv()
-    print("\nHello LangChain ReAct Agent!\n")
-    # res = get_text_length.invoke(input={"text": "topic", "topic": "hello world"})
+
+    user_query = "What is Pinecone and vecotr db and why is it necessary how is it useful in ml?"
+
+    loader = TextLoader("database/medium-blog-01.txt", encoding="utf-8")
+    documents = loader.load()
+    # print(documents[0].model_dump().keys())
+
+    print("splitting...")
+
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(documents)
+
+    print(f"{len(texts)} chunks")
+    # [print(i) for i in texts]
+
+    embeddings = OllamaEmbeddings(
+        model="gemma2:2b", # embedding dimension = 2304
+        temperature=0.0,
+        top_k=10,
+        top_p=0.5,
+        tfs_z=1.0,
+        repeat_penalty=1.5,
+        stop=["\n\n"],
+    )
+    # # embedding = embeddings.embed_documents(texts) # creates `len(texts)` same number of embeddings as chunks
+    # # [print(len(i)) for i in embedding] # prints the dimension of each embedding | dimension varies with each model
+
+    # # print(embeddings.embed_documents(texts))
+
+    # pc_vs = PineconeVectorStore.from_documents(
+    #     documents=texts,
+    #     embedding=embeddings,
+    #     index_name=os.environ["PINECONE_INDEX_NAME"]
+    # )
+    # print(pc_vs)
+
+    # query_template = PromptTemplate.from_template(
+    #     template=user_query,
+    #     # input_variables=["topic"],
+    # )
+
+    # llm = ChatOllama(
+    #     model="gemma2:2b",
+    #     temperature=0.0,
+    #     top_k=10,
+    #     top_p=0.5,
+    #     tfs_z=1.0,
+    #     repeat_penalty=1.5,
+    #     # stop=["\n\n"],
+    # )
+
+    # chain = query_template | llm
+
+    # res = chain.invoke(input={})
+    # print(res.content)  
     
-    user_input_to_llm = "What is the length of the text: dogg ? I only need the number"
-
-    harrison_chase_modified_prompt = """
-        You have access to the following tools:
-        {tools}
-
-        Please use the following format:
-
-        Question: the input question you must answer
-        Thought: Do I need to use a tool? Yes/No I need/need not to use a tool
-        Action: the action to take, should be one of [{tool_names}]
-        Action Input: the input to the action
-        Observation: the result of the action
-        ... (this Thought/Action/Action Input/Observation can repeat N times)
-
-        When you have a response to say to the Human, or if you do not need to use a tool, you MUST use the format:
-        {{
-        Thought: I know the final answer, Do I need to use a tool? No
-        Final Answer: [your response or final answer to the original input Question here]
-        }}
-        Do not attach backticks or quotes to any of your outputs, including thoughts, tool calls and final answers.
-
-        Begin!
-
-        Question: {input}
-        Thought: Do I need to use a tool? {agent_scratchpad}
+    """   
+    # ## Assusming the response is not accurate for the asked user_query, we would want to fetch the information from our medium-blog-01 for information
     """
-    # agent_scratchpad : brings back the previous thought process (history) of the agent
-    
+
+    vector_store = PineconeVectorStore(
+        index_name=os.environ["PINECONE_INDEX_NAME"],
+        embedding=embeddings,
+    )
     llm = ChatOllama(
         model="gemma2:2b",
-        temperature=0,
-        stop=["\nObservation"],
-        callbacks=[CustomAgentCallbackHandler()],
-        # prompt_template=prompt_template
+        temperature=0.0
     )
-    
-    tools = [
-        get_text_length
-    ]
-    
-    prompt_template = PromptTemplate.from_template(template=harrison_chase_modified_prompt).partial(
-        tools=render_text_description( tools), 
-        tool_names=", ".join([tool.name for tool in tools])
+    retrival_qa_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    custom_rag_prompt_template = """
+    Use the following pieces of context to answer the question at the end.
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    Use three to four sentences maximum and keep the answer as concise as possible.
+    Always say "Happy to help!" at the end of the answer.
+    {context}
+
+    Question: {question}
+    Helpful Answer:
+    """
+    combine_docs_chain = create_stuff_documents_chain(
+        llm=llm,
+        prompt=retrival_qa_prompt
     )
-    
-    intermediate_steps = []
-
-    agent = ( # | = pipe  LCEL (Langchain Expression Language) operator, which is a way to chain components together in LangChain
-        {
-            "input": lambda x: x["input"],
-            "agent_scratchpad": lambda x: format_log_str(x["agent_scratchpad"]),
-         } 
-         | prompt_template 
-         | llm 
-         | text_length_parser)
-    
-    print(agent)
-
-    """## # initial implementation of the agent executor by myself # ##
-    
-    agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
-        {
-            "input": user_input_to_llm,
-            "agent_scratchpad": intermediate_steps
-        }
+    retrival_chain = create_retrieval_chain(
+        retriever=vector_store.as_retriever(),
+        combine_docs_chain=combine_docs_chain,
     )
-    # print(agent_step)
-    print("Agent Executor Steps:")
-    print(f"{'-'*30}\n")
-    print(agent_step.model_dump().get("log"))
+    # res = retrival_chain.invoke(input={"input": user_query})
+    # # print(dir(res))
+    # print(res.get("answer"))
 
-    react_agent_step_counter = 1
-    if isinstance(agent_step, AgentAction):
-        while not isinstance(agent_step, AgentFinish):
-            react_agent_step_counter += 1
-            
-            tool_name = agent_step.tool
-            tool_to_use = find_tool_by_name(tools, tool_name)
-            tool_input = agent_step.tool_input
+    # ## LCEL Implementation
+    custom_rag_prompt = PromptTemplate(template=custom_rag_prompt_template)
+    rag_chain = (
+        {"context": vector_store.as_retriever() | format_docs, "question": RunnablePassthrough()}
+        | custom_rag_prompt
+        | llm
+    )
 
-            observation = tool_to_use.func(str(tool_input), "")  # 2nd argument is the topic of function get_text_length, passing blank as of now
-            # print(f"{observation=}")
-            intermediate_steps.append((agent_step, str(observation)))
-
-            # Re-invoke the agent with the updated intermediate steps and observations
-            agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
-                {
-                    "input": user_input_to_llm,
-                    "agent_scratchpad": intermediate_steps
-                }
-            )
-            print(agent_step.model_dump().get("log"))
-            if react_agent_step_counter > 5:
-                print("Too many thought processes, breaking out of the thinking.")
-                break
-        if isinstance(agent_step, AgentFinish):
-            print("-"*30)
-            print(agent_step.return_values.get("output"))
-        #############################################################################
-        """
-    
-    # easy while loop implementation of the agent executor
-    print("Agent Executor Steps:")
-    print(f"{'-'*30}\n")
-    agent_step = ""
-    react_agent_step_counter = 1
-    while not isinstance(agent_step, FinalAnswerModel):
-        react_agent_step_counter += 1
-        # Invoke the agent with the intermediate steps and observations, the loop will reinvoke the agent with the updated intermediate steps and observations
-        agent_step: Union[AgentAction, AgentFinish, FinalAnswerModel] = agent.invoke({
-                "input": user_input_to_llm,
-                "agent_scratchpad": intermediate_steps
-            }
-        )
-        if isinstance(agent_step, AgentAction):
-            print(agent_step.model_dump().get("log"))
-            
-            tool_name = agent_step.tool
-            tool_to_use = find_tool_by_name(tools, tool_name)
-            tool_input = agent_step.tool_input
-
-            observation = tool_to_use.func(str(tool_input), "")  # 2nd argument is the topic of custom function get_text_length which contains the business logic, passing blank as of now
-            intermediate_steps.append((agent_step, str(observation)))
-
-        if react_agent_step_counter > 5: # to avoid infinite loop
-            print("Too many thought processes, breaking out of the thinking.")
-            break
-            
-    if isinstance(agent_step, FinalAnswerModel):
-        print("-"*30)
-        print(agent_step.to_dict())
-        # print(agent_step.return_values.get("output"))
-    
-    # above is the same as below 
-    # from langchain.agents import initialize_agent, AgentType
-    # agent_executor = initialize_agent(
-    #     tools=tools,
-    #     llm=llm,
-    #     agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-    #     verbose=True,
-    #     agent_kwargs={
-    #         "agent_scratchpad": format_log_str(intermediate_steps)
-    #     },
-    # )
+    res = rag_chain.invoke( user_query)
+    print(res.content)
